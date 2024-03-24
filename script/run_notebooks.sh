b656cda7370b7b9d0fabdd07a434a65e90a6f43c
stgkrt <taro.stst@gmail.com>
2024-03-24 14:56:07 +0000

    :sparkles: run notebook scripts

---
+++ script/run_notebooks.sh
@@ -0,0 +1,3 @@
+jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-resnet34d.ipynb
+jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-effnetb0.ipynb
+jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-effnetb1.ipynb
