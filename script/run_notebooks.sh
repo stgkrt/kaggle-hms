e57165979b14cc540c27f2c9c9faaed84489b3dd
stgkrt <taro.stst@gmail.com>
2024-03-29 11:10:05 +0000

    :sparkles: notebook convert sh

--- script/run_notebooks.sh
+++ script/run_notebooks.sh
@@ -1,3 +1,8 @@
-jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-resnet34d.ipynb
-jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-effnetb0.ipynb
-jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-effnetb1.ipynb
+# jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-resnet34d.ipynb
+# jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-effnetb0.ipynb
+# jupyter nbconvert --to notebook --execute /kaggle/notebook/hms-train-effnetb1.ipynb
+# jupyter nbconvert --to notebook --execute /kaggle/notebook/features-head-starter-cvtest_E.ipynb
+# jupyter nbconvert --to notebook --execute /kaggle/notebook/features-head-starter-cvtest_R.ipynb
+jupyter nbconvert --to notebook --execute /kaggle/notebook/features-head-starter-cvtest_KR.ipynb
+jupyter nbconvert --to notebook --execute /kaggle/notebook/features-head-starter-cvtest_ER.ipynb
+jupyter nbconvert --to notebook --execute /kaggle/notebook/features-head-starter-cvtest_KER.ipynb
